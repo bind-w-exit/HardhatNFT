187881ac98fd64bb5598fd06d600ce133b711861
bind-w-exit <12Andriy24@Gmail.com>
2022-07-19 09:21:05 +0300

    Finished development of NftToken

--- contracts/NftToken.sol
+++ contracts/NftToken.sol
@@ -4,52 +4,60 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Creature is ERC721("My NFT", "MNFT"), Ownable {
+contract Creature is ERC721("My NFT Collection", "MNFTC"), Ownable {
+    using Strings for uint256;
 
-    mapping(uint256 => string) contentIdentifiers;
-    uint256 public nftPrice;
-    uint256 public lastTokenId;
+    uint256 constant public MAX_SUPPLY = 10;
+    string public baseURI;
+    uint256 public cost;
+    uint256 public totalSupply;
+
+    event SetBaseURI(string newBaseURI);
+    event SetCost(uint256 newCost);
+    event Buy(address indexed to, uint256 tokenId, uint256 cost);
+    event Withdraw(address indexed to, uint256 amount);
     
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs/";
+    
+    constructor(string memory initBaseURI) {
+        setBaseURI(initBaseURI);
     }
 
-    function _mint(address to, string calldata contentIdentifier) internal {
-        uint256 tokenId = lastTokenId++;
-        _safeMint(to, tokenId);
-        contentIdentifiers[tokenId] = contentIdentifier;
-        lastTokenId++;
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
 
-    function mint(address to, string calldata contentIdentifier) external onlyOwner {
-        _mint(to, contentIdentifier);
-    }
-
-    function burn(uint256 tokenId) external onlyOwner {
-        _burn(tokenId);
-        contentIdentifiers[tokenId] = "";
-    }
-
-    function tokenURI(uint256 tokenId) external view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
 
-        string memory baseURI = _baseURI();
-        string memory contentIdentifier = contentIdentifiers[tokenId];
-
-        if(bytes(baseURI).length > 0 || bytes(contentIdentifier).length > 0) {
-            return string(abi.encodePacked(baseURI, contentIdentifier));
-        } else {
-            return "";
-        }
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), ".json")) : "";
     }
 
-    function setNftPrice(uint256 price) external onlyOwner {
-        nftPrice = price;
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+        baseURI = newBaseURI;
+        emit SetBaseURI(newBaseURI);
     }
 
-    function buy(string calldata contentIdentifier) external payable {
-        require(msg.value == nftPrice, "NFT: incorrect amount");
-        _mint(msg.sender, contentIdentifier);
+    function setCost(uint256 newCost) external onlyOwner {
+        cost = newCost;
+        emit SetCost(newCost);
     }
 
+    function buy() external payable {
+        require(totalSupply + 1 <= MAX_SUPPLY);
+        require(msg.value == cost, "NFT: incorrect amount");
+
+        uint256 tokenId = totalSupply++;
+        _safeMint(msg.sender, tokenId);
+        totalSupply++;
+
+        emit Buy(msg.sender, tokenId, cost);
+    }
+
+    function withdraw() external onlyOwner { 
+        uint256 amountToSend = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amountToSend}("");
+        require(success, "NftToken: unable to withdraw, recipient may have reverted");
+        emit Withdraw(msg.sender, amountToSend);
+    }
 }
